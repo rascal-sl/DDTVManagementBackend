04023a4124613bdb427f92fb5e56a426bd49e7a5
Roshan Reji <roshanreji987@gmail.com>
2025-06-27 05:23:41 +0530

    small addon for role related status issues

--- routes/repair.routes.js
+++ routes/repair.routes.js
@@ -7,13 +7,13 @@ const { authorizeRoles } = require('../middleware/role.middleware');
 
 // Status Master CRUD
 router.post('/statuses', authenticateToken, authorizeRoles('super_admin'), statusCtrl.createStatus);
-router.get('/statuses', authenticateToken, authorizeRoles('super_admin'), statusCtrl.getStatuses);
+router.get('/statuses', authenticateToken, authorizeRoles('admin','super_admin'), statusCtrl.getStatuses);
 router.put('/statuses/:id', authenticateToken, authorizeRoles('super_admin'), statusCtrl.updateStatus);
 router.delete('/statuses/:id', authenticateToken, authorizeRoles('super_admin'), statusCtrl.deleteStatus);
 
 // Issue Master CRUD
 router.post('/issues', authenticateToken, authorizeRoles('super_admin'), issueCtrl.createIssue);
-router.get('/issues', authenticateToken, authorizeRoles('super_admin'), issueCtrl.getIssues);
+router.get('/issues', authenticateToken, authorizeRoles('admin', 'super_admin'), issueCtrl.getIssues);
 router.put('/issues/:id', authenticateToken, authorizeRoles('super_admin'), issueCtrl.updateIssue);
 router.delete('/issues/:id', authenticateToken, authorizeRoles('super_admin'), issueCtrl.deleteIssue);
 
